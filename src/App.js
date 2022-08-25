15f5a6ee261dad0f7bf7d9a17dcb6c9d795d318c
Martin Torrey <hockeymt.torrey@gmail.com>
2022-08-25 14:07:07 -0400

    adding everything to github

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {NavBar} from './components/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { Banner } from './components/Banner'
+import { Skills } from './components/Skills'
 
 function App() {
   return (
     <div className="App">
         <NavBar></NavBar>
         <Banner />
+        <Skills />
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,7 +7,7 @@ import headerImg from "../assets/img/header-img.svg"
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["pocket book","mom's spaghetti","homemade chili"]
+    const toRotate = ["Exciting","Daunting","Terrifying"]
     const [text,setText] = useState('')
     const [delta, setDelta] = useState(300-Math.random()*100)
     const period = 2000;
@@ -44,8 +44,12 @@ export const Banner = () => {
                 <Row className="align-items-center">
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to the React Playground</span>
-                        <h1>{`Oops, you dropped your `}<span className="wrap">{text}</span></h1>
-                        <p>The FitnessGram™ Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start.</p>
+                        <h1>{`Space: `}<span className="wrap">{text}</span></h1>
+                        <p>Space is the boundless three-dimensional extent in which objects and events have relative position and direction.[1] In classical physics, physical space is often conceived in three linear dimensions, although modern physicists usually consider it, with time, to be part of a boundless four-dimensional continuum known as spacetime. The concept of space is considered to be of fundamental importance to an understanding of the physical universe. However, disagreement continues between philosophers over whether it is itself an entity, a relationship between entities, or part of a conceptual framework.
+
+</p><p>Debates concerning the nature, essence and the mode of existence of space date back to antiquity; namely, to treatises like the Timaeus of Plato, or Socrates in his reflections on what the Greeks called khôra (i.e. "space"), or in the Physics of Aristotle (Book IV, Delta) in the definition of topos (i.e. place), or in the later "geometrical conception of place" as "space qua extension" in the Discourse on Place (Qawl fi al-Makan) of the 11th-century Arab polymath Alhazen.[2] Many of these classical philosophical questions were discussed in the Renaissance and then reformulated in the 17th century, particularly during the early development of classical mechanics. In Isaac Newton's view, space was absolute—in the sense that it existed permanently and independently of whether there was any matter in the space.[3] Other natural philosophers, notably Gottfried Leibniz, thought instead that space was in fact a collection of relations between objects, given by their distance and direction from one another. In the 18th century, the philosopher and theologian George Berkeley attempted to refute the "visibility of spatial depth" in his Essay Towards a New Theory of Vision. Later, the metaphysician Immanuel Kant said that the concepts of space and time are not empirical ones derived from experiences of the outside world—they are elements of an already given systematic framework that humans possess and use to structure all experiences. Kant referred to the experience of "space" in his Critique of Pure Reason as being a subjective "pure a priori form of intuition".
+
+</p><p>In the 19th and 20th centuries mathematicians began to examine geometries that are non-Euclidean, in which space is conceived as curved, rather than flat. According to Albert Einstein's theory of general relativity, space around gravitational fields deviates from Euclidean space.[4] Experimental tests of general relativity have confirmed that non-Euclidean geometries provide a better model for the shape of space.</p>
                         <button onClick={() => console.log('connect')}>Let's connect<ArrowRightCircle size={25}></ArrowRightCircle></button>
                     </Col>
                     <Col xs={12} md={6} xl={5}>
